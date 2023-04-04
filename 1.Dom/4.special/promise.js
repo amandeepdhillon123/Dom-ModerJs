4b910b6ba39ecb3cedb3c5c5cc29911c4a4b5133
amandeepdhillon123 <amandeep24397@gmail.com>
2023-04-04 06:05:58 +0530

    create promise

---
+++ 1.Dom/4.special/promise.js
@@ -0,0 +1,10 @@
+
+let hello = new Promise(function(resolve,reject) {
+
+    console.log("hello amandeep");
+    resolve();
+    // reject( new Error("bhai rejected"));
+});
+
+console.log("amandeep")
+console.log("amandeep")
